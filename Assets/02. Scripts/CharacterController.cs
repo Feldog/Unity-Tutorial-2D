f768cfbb0f323bbc613077f7b1968e24e06da99d
Feldog <hoya0895@naver.com>
2025-05-20 13:00:06 +0900

    first commit

---
+++ Assets/02. Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float moveSpeed = 5f;
+    public float moveX;
+    public float moveY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(moveX, 0, moveY);
+        movement.Normalize();
+
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/02. Scripts/StudyLog.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class StudyLog : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("StudyLog Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("StudyLog Update");
+    }
+}
